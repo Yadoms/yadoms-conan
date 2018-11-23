8175f5eeae042150a9d19c6b104e146f4bda0665
Jano42 <decoretjm@gmail.com>
2018-11-23 16:03:04 +0100

    Fix conanfile fix command line

--- conan_install_windows.cmd
+++ conan_install_windows.cmd
@@ -1,5 +1,5 @@
 cd build
-conan install --build missing -s compiler.runtime=MT -s arch=x86 -s build_type=Debug ..
+conan install --build missing -s compiler.runtime=MTd -s arch=x86 -s build_type=Debug ..
 conan install --build missing -s compiler.runtime=MT -s arch=x86 -s build_type=Release ..
 cd ..
 
